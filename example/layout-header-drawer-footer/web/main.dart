c4f1be8f37b832ff0e369480ffa8c0c5606c4e9f
Mike Mitterer <office@mikemitterer.at>
2015-03-04 16:54:35 +0100

    doc: Two new sample-layouts

---
+++ example/layout-header-drawer-footer/web/main.dart
@@ -0,0 +1,58 @@
+import 'dart:html' as html;
+
+import 'package:logging/logging.dart';
+import 'package:console_log_handler/console_log_handler.dart';
+
+import 'package:wsk_material/wskcomponets.dart';
+
+void main() {
+    html.querySelector("body").classes.add("update-theme");
+    configLogging();
+
+    scrollChecker();
+
+    registerAllWskComponents();
+    upgradeAllRegistered();
+    html.querySelector("body").classes.remove("update-theme");
+}
+
+void configLogging() {
+    hierarchicalLoggingEnabled = false; // set this to true - its part of Logging SDK
+
+    // now control the logging.
+    // Turn off all logging first
+    Logger.root.level = Level.INFO;
+    Logger.root.onRecord.listen(new LogConsoleHandler());
+}
+
+void scrollChecker() {
+    final html.HtmlElement body = html.querySelector("body");
+    final html.HtmlElement content = html.querySelector(".wsk-layout__content");
+    final html.HtmlElement shadow = html.querySelector(".addscrollshadow");
+    final html.ButtonElement button = html.querySelector("#totop");
+
+    if(content == null || shadow == null || button == null) {
+        return;
+    }
+
+    button.onClick.listen((_) {
+        content.scrollTop = 0;
+    });
+
+    content.onScroll.listen((final html.Event event) {
+        final int top = content.scrollTop;
+
+        if(top > 25) {
+            shadow.classes.add("wsk-shadow--z2");
+        } else {
+            shadow.classes.remove("wsk-shadow--z2");
+        }
+
+        if(top > 100) {
+            body.classes.add("add-back-to-top-button");
+        } else {
+            body.classes.remove("add-back-to-top-button");
+        }
+
+    });
+}
