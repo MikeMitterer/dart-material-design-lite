dfc4faff20822a9261692bbcca4d426aa27b3354
Mike Mitterer <office@mikemitterer.at>
2015-04-13 15:45:11 +0200

    List had relative path to avatar

--- tool/gensamples.dart
+++ tool/gensamples.dart
@@ -137,7 +137,7 @@ class Application {
 
         _copy("_${sampleName}","scss",true);
         _copy("demo","html",false);
-        _copy("demo","scss",sampleName == "typography"); // nur bei typography wird ein demo.orig.scss erstellt
+        _copy("demo","scss",sampleName == "typography" || sampleName == "list"); // nur bei typography wird ein demo.orig.scss erstellt
         _copy("README","md",false);
 
         final File srcJS = new File("${mdlSampleDir.path}/${sampleName}.js");
