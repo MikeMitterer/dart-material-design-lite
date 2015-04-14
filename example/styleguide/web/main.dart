2280833869614c7a6d08582e6fa0c4c11ebd593b
Mike Mitterer <office@mikemitterer.at>
2015-04-14 11:38:55 +0200

    Styleguide + sitegen works, styleguide-title is updated automatically

--- example/styleguide/web/main.dart
+++ example/styleguide/web/main.dart
@@ -23,17 +23,17 @@ class Model {
 
     Stream<ModelChangedEvent> onChange;
 
-    int _sliderValue = 20;
+    String _title = "";
 
     factory Model() {
         if(_model == null) {  _model = new Model._internal(); }
         return _model;
     }
 
-    int get sliderValue => _sliderValue;
+    String get title => _title;
 
-    set sliderValue(final int value) {
-        _sliderValue = value;
+    set title(final String value) {
+        _title = value;
         _controller.add(new ModelChangedEvent());
     }
 
@@ -58,23 +58,21 @@ main() {
     // registerDemoAnimation and import wskdemo.dart is on necessary for animation sample
     registerDemoAnimation();
 
-
     upgradeAllRegistered().then((_) {
-
+        model.onChange.listen((_) {
+            dom.querySelector("#title").text = model.title;
+        });
     });
 }
 
 class DemoController extends MaterialController {
     final Model _model = new Model();
 
-
     @override
     void loaded(final Route route) {
-
+        _model.title = route.name;
     }
-
     // - private ------------------------------------------------------------------------------------------------------
-
 }
 
 void configRouter() {
@@ -84,83 +82,83 @@ void configRouter() {
     router.root
 
         ..addRoute(name: 'accordion', path: '/accordion',
-                    enter: view("views/accordion.html", new DummyController()))
+                    enter: view("views/accordion.html", new DemoController()))
 
         ..addRoute(name: 'animation', path: '/animation',
-                    enter: view("views/animation.html", new DummyController()))
+                    enter: view("views/animation.html", new DemoController()))
 
         ..addRoute(name: 'badge', path: '/badge',
-                    enter: view("views/badge.html", new DummyController()))
+                    enter: view("views/badge.html", new DemoController()))
 
         ..addRoute(name: 'button', path: '/button',
-                    enter: view("views/button.html", new DummyController()))
+                    enter: view("views/button.html", new DemoController()))
 
         ..addRoute(name: 'card', path: '/card',
-                    enter: view("views/card.html", new DummyController()))
+                    enter: view("views/card.html", new DemoController()))
 
         ..addRoute(name: 'checkbox', path: '/checkbox',
-                    enter: view("views/checkbox.html", new DummyController()))
+                    enter: view("views/checkbox.html", new DemoController()))
 
         ..addRoute(name: 'column-layout', path: '/column-layout',
-                    enter: view("views/column-layout.html", new DummyController()))
+                    enter: view("views/column-layout.html", new DemoController()))
 
         ..addRoute(name: 'footer', path: '/footer',
-                    enter: view("views/footer.html", new DummyController()))
+                    enter: view("views/footer.html", new DemoController()))
 
         ..addRoute(name: 'icon-toggle', path: '/icon-toggle',
-                    enter: view("views/icon-toggle.html", new DummyController()))
+                    enter: view("views/icon-toggle.html", new DemoController()))
 
         ..addRoute(name: 'layout', path: '/layout',
-                    enter: view("views/layout.html", new DummyController()))
+                    enter: view("views/layout.html", new DemoController()))
 
         ..addRoute(name: 'list', path: '/list',
-                    enter: view("views/list.html", new DummyController()))
+                    enter: view("views/list.html", new DemoController()))
 
         ..addRoute(name: 'menu', path: '/menu',
-                    enter: view("views/menu.html", new DummyController()))
+                    enter: view("views/menu.html", new DemoController()))
 
         ..addRoute(name: 'nav-pills', path: '/nav-pills',
-            enter: view("views/nav-pills.html", new DummyController()))
+            enter: view("views/nav-pills.html", new DemoController()))
 
         ..addRoute(name: 'palette', path: '/palette',
-                    enter: view("views/palette.html", new DummyController()))
+                    enter: view("views/palette.html", new DemoController()))
 
         ..addRoute(name: 'panel', path: '/panel',
-            enter: view("views/panel.html", new DummyController()))
+            enter: view("views/panel.html", new DemoController()))
 
         ..addRoute(name: 'progress', path: '/progress',
-                    enter: view("views/progress.html", new DummyController()))
+                    enter: view("views/progress.html", new DemoController()))
 
         ..addRoute(name: 'radio', path: '/radio',
-                    enter: view("views/radio.html", new DummyController()))
+                    enter: view("views/radio.html", new DemoController()))
 
         ..addRoute(name: 'shadow', path: '/shadow',
-                    enter: view("views/shadow.html", new DummyController()))
+                    enter: view("views/shadow.html", new DemoController()))
 
         ..addRoute(name: 'slider', path: '/slider',
-                    enter: view("views/slider.html", new DummyController()))
+                    enter: view("views/slider.html", new DemoController()))
 
         ..addRoute(name: 'spinner', path: '/spinner',
-                    enter: view("views/spinner.html", new DummyController()))
+                    enter: view("views/spinner.html", new DemoController()))
 
         ..addRoute(name: 'switch', path: '/switch',
-                    enter: view("views/switch.html", new DummyController()))
+                    enter: view("views/switch.html", new DemoController()))
 
         ..addRoute(name: 'tabs', path: '/tabs',
-                    enter: view("views/tabs.html", new DummyController()))
+                    enter: view("views/tabs.html", new DemoController()))
 
         ..addRoute(name: 'textfield', path: '/textfield',
-                    enter: view("views/textfield.html", new DummyController()))
+                    enter: view("views/textfield.html", new DemoController()))
 
         ..addRoute(name: 'tooltip', path: '/tooltip',
-                    enter: view("views/tooltip.html", new DummyController()))
+                    enter: view("views/tooltip.html", new DemoController()))
 
         ..addRoute(name: 'typography', path: '/typography',
-            enter: view("views/typography.html", new DummyController()))
+            enter: view("views/typography.html", new DemoController()))
     
 
         ..addRoute(name: 'home', defaultRoute: true, path: '/',
-            enter: view("views/home.html" ,new DummyController()))
+            enter: view("views/home.html" ,new DemoController()))
 
     ;
 
