83a7ad7ca43272c52264e06291388020fd63f223
Mike Mitterer <office@mikemitterer.at>
2015-04-16 10:55:45 +0200

    Icon-Toggle-Sample works in Styleguide

--- example/styleguide/web/main.dart
+++ example/styleguide/web/main.dart
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2015, Michael Mitterer (office@mikemitterer.at),
+ * IT-Consulting and Development Limited.
+ *
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import "dart:html" as dom;
 import "dart:async";
 
@@ -95,10 +114,24 @@ class BadgeController extends DemoController {
             counter++;
         });
     }
-
     // - private ------------------------------------------------------------------------------------------------------
 }
 
+class IconToggleController extends DemoController {
+    final Logger _logger = new Logger('main.IconToggleController');
+
+    @override
+    void loaded(final Route route) {
+        super.loaded(route);
+
+        final MaterialIconToggle toggle = MaterialIconToggle.widget(dom.querySelector("#public-checkbox-1"));
+        new Timer.periodic(new Duration(milliseconds: 500), (final Timer timer) {
+            toggle.checked = !toggle.checked;
+        });
+
+    }
+// - private ------------------------------------------------------------------------------------------------------
+}
 
 void configRouter() {
     final Router router = new Router(useFragment: true);
@@ -134,7 +167,7 @@ void configRouter() {
             enter: view("views/icons.html", new DemoController()))
 
         ..addRoute(name: 'icon-toggle', path: '/icon-toggle',
-                    enter: view("views/icon-toggle.html", new DemoController()))
+                    enter: view("views/icon-toggle.html", new IconToggleController()))
 
         ..addRoute(name: 'layout', path: '/layout',
                     enter: view("views/layout.html", new DemoController()))
