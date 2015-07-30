8f8f0a547eda9f10b56daf7c54e0aacf14b464da
Mike Mitterer <office@mikemitterer.at>
2015-07-30 10:11:30 +0200

    Removed dyn-sample

--- example/mdlx_accordion/web/main.dart
+++ example/mdlx_accordion/web/main.dart
@@ -5,26 +5,14 @@ import 'package:console_log_handler/console_log_handler.dart';
 
 import 'package:mdl/mdl.dart';
 
-main() async {
+main() {
     configLogging();
 
     registerMdl();
 
-    await componentFactory().run();
+    componentFactory().run().then((_) {
 
-    final dom.ButtonElement btn = new dom.ButtonElement()
-        ..text = "Click me"
-        ..id = "mybutton"
-    ;
-
-    btn.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored";
-    btn.style.marginTop = "12px";
-
-    await componentHandler().upgradeElement(btn);
-    dom.querySelector(".demo-section").append(btn);
-
-    final MaterialButton button = MaterialButton.widget(dom.querySelector("#mybutton"));
-    button.disable();
+    });
 }
 
 void configLogging() {
